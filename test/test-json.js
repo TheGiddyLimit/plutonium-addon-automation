25cb95651c6ecd8af2acdeb93033bc6b9ea5469a
TheGiddyLimit <TheGiddyLimit@5etools.com>
2022-04-03 23:55:47 +0100

    feat(sample): add `_meta` sections to homebrew files
    This demonstrates a simple `_meta` addon which has the "important bits"
    of the standard homebrew `_meta`, in a slightly-flattened format (this
    is due to each homebrew file in this repo matching exactly one homebrew
    `"json"` source, whereas a file in the homebrew repo can contain
    multiple `"json"` sources).
    
    We choose to omit the `author`, `url`, etc. properties, as this
    information is already stored in the homebrew repo, and duplicating it
    has no benefit.

--- test/test-json.js
+++ test/test-json.js
@@ -2,6 +2,8 @@ import Ajv from "ajv";
 import * as jsonSourceMap from "json-source-map";
 import * as kludge from "../script/kludge.js";
 
+console.log(`Testing JSON files against schema.`);
+
 const ajv = new Ajv({
 	allowUnionTypes: true,
 });
@@ -11,15 +13,22 @@ ajv.addKeyword({
 	validate: false,
 });
 
-const schema = kludge.readJsonSync("./test/schema/data.json");
+kludge.lsRecursiveSync("./test/schema")
+	.forEach(it => {
+		const schema = kludge.readJsonSync(it);
+		ajv.addSchema(schema, it.$id);
+	});
 
 let isFail = false;
 kludge.lsRecursiveSync("./module/data")
 	.filter(it => !it.includes("_generated") && it.endsWith(".json"))
 	.forEach(path => {
-		console.log(`Testing ${path}`);
+		const isCore = path.includes("__core.json");
+
+		console.log(`\tTesting (${isCore ? "core" : "brew"}) ${path}`);
+
 		const data = kludge.readJsonSync(path);
-		const valid = ajv.validate(schema, data);
+		const valid = ajv.validate(isCore ? "core.json" : "homebrew.json", data);
 		if (valid) return;
 
 		// Add line numbers
@@ -30,7 +39,9 @@ kludge.lsRecursiveSync("./module/data")
 			it.lineNumberEnd = errorPointer.valueEnd.line;
 		});
 
-		const out = ajv.errors.map(it => JSON.stringify(it, null, 2)).join("\n");
+		const out = ajv.errors.map(it => JSON.stringify(it, null, 2))
+			.map(str => str.split("\n").map(l => `\t${l}`).join("\n"))
+			.join("\n");
 		console.error(out);
 		isFail = true;
 	});
