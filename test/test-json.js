11f17ff795caafbe5b93823ff01168edddfbbc8d
TheGiddyLimit <TheGiddyLimit@5etools.com>
2022-04-07 21:24:21 +0100

    feat(test): use 5etools library for JSON test implementation
    This avoids duplicating existing work.

--- test/test-json.js
+++ test/test-json.js
@@ -1,53 +1,23 @@
-import Ajv from "ajv";
-import * as jsonSourceMap from "json-source-map";
-import * as kludge from "../script/kludge.js";
+import * as fs from "fs";
 
-console.log(`Testing JSON files against schema.`);
+import {Um, JsonTester} from "5etools-utils";
 
-const ajv = new Ajv({
-	allowUnionTypes: true,
-});
+function main () {
+	const {errors, errorsFull} = new JsonTester(
+		{
+			dirSchema: "test/schema",
+			dir: "module/data",
+			fnGetSchemaId: path => path.includes("__core.json") ? "core.json" : "homebrew.json",
+		},
+	).getErrors();
 
-ajv.addKeyword({
-	keyword: "version",
-	validate: false,
-});
+	if (errors.length) {
+		if (!process.env.CI) fs.writeFileSync(`test/test-json.error.log`, errorsFull.join("\n\n=====\n\n"));
+		console.error(`Schema test failed (${errors.length} failure${errors.length === 1 ? "" : "s"}).`);
+		process.exit(1);
+	}
 
-kludge.lsRecursiveSync("./test/schema")
-	.forEach(it => {
-		const schema = kludge.readJsonSync(it);
-		ajv.addSchema(schema, it.$id);
-	});
-
-let isFail = false;
-kludge.lsRecursiveSync("./module/data")
-	.filter(it => !it.includes("_generated") && it.endsWith(".json"))
-	.forEach(path => {
-		const isCore = path.includes("__core.json");
-
-		console.log(`\tTesting (${isCore ? "core" : "brew"}) ${path}`);
-
-		const data = kludge.readJsonSync(path);
-		const valid = ajv.validate(isCore ? "core.json" : "homebrew.json", data);
-		if (valid) return;
-
-		// Add line numbers
-		const sourceMap = jsonSourceMap.stringify(data, null, "\t");
-		ajv.errors.forEach(it => {
-			const errorPointer = sourceMap.pointers[it.instancePath];
-			it.lineNumberStart = errorPointer.value.line;
-			it.lineNumberEnd = errorPointer.valueEnd.line;
-		});
-
-		const out = ajv.errors.map(it => JSON.stringify(it, null, 2))
-			.map(str => str.split("\n").map(l => `\t${l}`).join("\n"))
-			.join("\n");
-		console.error(out);
-		isFail = true;
-	});
-
-if (isFail) {
-	console.error(`Files failed schema test!`);
-	process.exit(1);
+	if (!errors.length) Um.info("JSON", `Schema test passed.`);
 }
-console.log(`All files passed.`);
+
+main();
