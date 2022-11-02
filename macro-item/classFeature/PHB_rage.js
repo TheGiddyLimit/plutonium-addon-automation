b0feab6aac46e1c7e4093dc87bcee155dae266ef
TheGiddyLimit <TheGiddyLimit@5etools.com>
2022-11-02 14:01:02 +0000

    chore: lint

--- macro-item/classFeature/PHB_rage.js
+++ macro-item/classFeature/PHB_rage.js
@@ -1,12 +1,11 @@
-async function macro(args) {
-  const token = await fromUuid(args[1].tokenUuid)
-  const tactor = token.actor
+async function macro (args) {
+	const token = await fromUuid(args[1].tokenUuid);
+	const tactor = token.actor;
 
-  const barb = tactor.items.getName("Barbarian")
-  const subclass = barb?.subclass
+	const barb = tactor.items.getName("Barbarian");
+	const subclass = barb?.subclass;
 
-  if (!subclass || !subclass?.hasMacro())
-    return
+	if (!subclass || !subclass?.hasMacro()) { return; }
 
-  subclass.executeMacro(...args)
+	subclass.executeMacro(...args);
 }
